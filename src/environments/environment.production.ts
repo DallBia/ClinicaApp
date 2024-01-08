0a397da18183cfc20c748cd0a47c134fb770e845
DallBia <146030584+DallBia@users.noreply.github.com>
2024-01-08 19:29:08 -0300

    Update environment.production.ts

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,15 +1,12 @@
 
+
 // export const environment = {
 //   ApiUrl: "https://localhost:7298/api"
 // };
 
 
 
-// export const environment = {
-//   production: true,
-//   ApiUrl: 'http://34.67.223.72:80/api'
-// };
 export const environment = {
   production: true,
-  ApiUrl: 'http://35.223.137.132:80/api'
+  ApiUrl: 'http://35.232.35.159/api'
 };
