8e417051e9e82a68aadebe55f03e6c3bd66598cb
Bi-Dall <bianchini.dallacqua@gmail.com>
2024-02-09 11:43:06 -0300

    datas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 
 
-export const environment = {
-  ApiUrl: "https://localhost:7298/api"
-};
-
-
-
 // export const environment = {
-//   production: true,
-//   ApiUrl: 'https://api.clincasagrande.com.br/api'
+//   ApiUrl: "https://localhost:7298/api"
 // };
+
+
+
+export const environment = {
+  production: true,
+  ApiUrl: 'https://api.clincasagrande.com.br/api'
+};
 
--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,12 +1,12 @@
 
 
-export const environment = {
-  ApiUrl: "https://localhost:7298/api"
-};
-
-
 // export const environment = {
-//   production: true,
-//   ApiUrl: 'https://api.clincasagrande.com.br/api'
+//   ApiUrl: "https://localhost:7298/api"
 // };
+
+
+export const environment = {
+  production: true,
+  ApiUrl: 'https://api.clincasagrande.com.br/api'
+};
 
